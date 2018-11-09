f02570fd16f48ce93fe0464032dcecaba301a5d7
Samuel Paul Christopher <thesamchris@gmail.com>
2018-11-09 06:01:19 +0000

    trying to get server running

--- server/index.js
+++ server/index.js
@@ -1,1 +1,39 @@
-console.log(process.env.T_AUTH_TOKEN)
+let express = require('express')
+let firebase = require('firebase/app')
+require('firebase/database')
+let app = express()
+
+app.set('default engine', 'pug')
+
+app.get('/', (req, res) => {
+  return res.render('index.pug')
+})
+
+app.post('/', (req, res) => {
+  console.log(req)
+})
+
+const config = {
+  apiKey: process.env.F_API_KEY,
+  authDomain: process.env.F_AUTH_DOMAIN,
+  databaseURL: process.env.F_DATABASE_URL,
+  storageBucket: process.env.F_STORAGE_BUCKET
+}
+
+firebase.initializeApp(config)
+let database = firebase.database()
+let customersRef = database.ref('customers/')
+// let newCustomerRef = customersRef.push()
+
+// let priyaDetails = {
+//   firstName: 'Priya',
+//   lastName: 'Christopher',
+//   paymentDateline: `${new Date('11 09 2018')}`,
+//   email: 'priya.satishc@gmail.com'
+// }
+
+// newCustomerRef.set(priyaDetails, function(err) {
+//   err ? console.log(err) : console.log('added details: ', priyaDetails)
+// })
+
+app.listen('3000', (err) => console.log('listening on port 3000', err))
